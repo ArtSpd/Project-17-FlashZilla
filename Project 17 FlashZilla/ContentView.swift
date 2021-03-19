d01717f9d7497d3cae028254d5121ce7d0440e69
ArtSpd <76838598+ArtSpd@users.noreply.github.com>
2021-03-19 17:26:31 +0300

    Initial Commit

---
+++ Project 17 FlashZilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 17 FlashZilla
+//
+//  Created by Артем Волков on 19.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
